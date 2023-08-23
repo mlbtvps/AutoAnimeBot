e060cd95ed7332344f2ccf7d2227322b95689bef
mlbtvps <119069067+mlbtvps@users.noreply.github.com>
2023-08-23 16:36:42 +0630

    Update tg_handler.py

--- AutoAnimeBot/modules/tg_handler.py
+++ AutoAnimeBot/modules/tg_handler.py
@@ -42,7 +42,7 @@ async def tg_handler(appp, TECHZ_API_KEY):
     app = appp
     queue = app.queue
     Gogo = TechZApi.Gogo(TECHZ_API_KEY)
-    Gogo.base = "https://api1.techzbots.live"
+    Gogo.base = "https://api.techzbots.live"
 
     while True:
         if len(queue) != 0:

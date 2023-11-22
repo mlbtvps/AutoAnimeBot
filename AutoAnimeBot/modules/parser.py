10c7e20d9e44a2bfd100c2beda26e52c2a56f6fc
mlbtvps <119069067+mlbtvps@users.noreply.github.com>
2023-11-23 06:26:05 +0630

    Update parser.py

--- AutoAnimeBot/modules/parser.py
+++ AutoAnimeBot/modules/parser.py
@@ -10,7 +10,7 @@ logger = LOGGER("Parser")
 
 async def auto_parser(TECHZ_API_KEY, app):
     Gogo = TechZApi.Gogo(TECHZ_API_KEY)
-    Gogo.base = "https://api-techzbots.koyeb.app"
+    Gogo.base = "https://animetopia-api-0cdbd873a81a.herokuapp.com"
 
     while True:
         await app.update_status("Scrapping Animes...")
